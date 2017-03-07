6cba387bc0395c2cf7b3ca11828b013b29f3484b
Thomas Sorrel <thomas@pandeiro.fr>
2017-03-07 16:02:06 +0100

    rename in_env in-env

--- sh/func.sh
+++ sh/func.sh
@@ -1,10 +1,10 @@
-# in_venv is a launcher of command within a Python virtualenv
+# in-venv is a launcher of command within a Python virtualenv
 # usage:
-#  $ in_venv <vitual env name> <command to launch>
-#  $ in_venv TOX tox
-#  $ in_venv TOX tox -e py34 -r
-#  $ in_venv dev python -m pdb my_app.py
-in_venv(){
+#  $ in-venv <vitual env name> <command to launch>
+#  $ in-venv TOX tox
+#  $ in-venv TOX tox -e py34 -r
+#  $ in-venv dev python -m pdb my_app.py
+in-venv(){
     (
         VENV=$1
         shift
